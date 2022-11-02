6360ca96e880f1c72471432f167f7ffffe626341
arostkow <arostkow@umich.edu>
2022-11-02 11:35:44 -0400

    fixed styling for login and sign up

--- eecs497-ttyy/src/views/Landing.vue
+++ eecs497-ttyy/src/views/Landing.vue
@@ -1,7 +1,14 @@
 <template>
-  <header/>
     <main>
-      
+    <div class="bar">
+        <div class="left">
+            <h1> TTYY </h1>
+        </div>
+        <div class="right">
+            <button v-on:click = "login()">Login</button>
+            <button v-on:click = "createAccount()">Sign Up</button>
+        </div>
+    </div>
       <div id="landing_page_1">
         <div id="landing_page_1_text">
           <h2>Find your next</h2>
@@ -38,6 +45,29 @@
     font-weight: 1000;
     color: #463538;
   }
+
+      .bar {
+        margin-top: 45px;
+        margin-bottom: 40px;
+        display: flex;
+        align-items: center;
+        border-bottom: #4d4d4f;
+    }
+
+
+    .bar .left {
+        position: absolute;
+        left: 50px;
+        padding-top: 18px;
+        padding-bottom: 25px;
+    }
+
+.bar .right {
+    position: absolute;
+    right: 50px;
+    /* font-weight: bold;
+    color: #4d4d4f; */
+}
   
   </style>
   
@@ -50,8 +80,12 @@
       function login(){
           router.push("/login");
       }
+
+      function createAccount(){
+          router.push("/create");
+      }
       
-      return { login, }
+      return { login, createAccount}
     },
   }
   </script>
--- eecs497-ttyy/src/views/Login.vue
+++ eecs497-ttyy/src/views/Login.vue
@@ -1,8 +1,9 @@
 <template>
+
+  <main>
   <div id="app">
     <div id="alert" v-if="alert">{{ alert }}</div>
-
-    <div class="bar">
+      <div class="bar">
         <div class="left">
             <h1> TTYY </h1>
         </div>
@@ -11,6 +12,7 @@
             <button v-on:click = "createAccount()">Sign Up</button>
         </div>
     </div>
+
     <h2>Login </h2>
     <form @submit.prevent="loginWithPassword">
       <label>
@@ -24,25 +26,14 @@
       <button type="submit">Log in</button>
     </form>
   </div>
+  </main>
 
 </template>
 
 <style scoped>
-  button,
-  input {
-    display: block;
-    margin-bottom: 10px;
-  }
 
-  body{
-    background-color: #F5CAC3;
-  }
 
-  #alert {
-    color: red;
-    margin-bottom: 10px;
-  }
-    .bar {
+        .bar {
         margin-top: 45px;
         margin-bottom: 40px;
         display: flex;
@@ -64,4 +55,22 @@
     /* font-weight: bold;
     color: #4d4d4f; */
 }
+
 </style>
+
+<script>
+  import router from "@/router";
+  export default { 
+    setup(){
+      function login(){
+          router.push("/login");
+      }
+
+      function createAccount(){
+          router.push("/create");
+      }
+      
+      return { login, createAccount}
+    },
+  }
+</script>
